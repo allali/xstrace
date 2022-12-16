727d197bdfaf61b31f66e6a02a96f924cf239cf3
Julien Allali <allali@enseirb-matmeca.fr>
2022-12-16 12:10:23 +0100

    complete fork support, add more complexe fork example

--- sample/fork.c
+++ sample/fork.c
@@ -1,11 +1,27 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 int main(){
-
+  char buffer[100];
+  int fd,e;
+  int p[2];
+  pipe(p);
+  
   if (fork()==0){
+    fd=open("in",O_RDONLY);
+    close(p[0]);
+    while((e=read(fd,buffer,100))>0){
+      write(p[1],buffer,e);
+    }    
     return 10;
+  }
+  close(p[1]);
+  while((e=read(p[0],buffer,100))>0){
+    write(1,buffer,e);
   }
   wait(NULL);
   return 0;
